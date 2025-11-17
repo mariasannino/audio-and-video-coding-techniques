429974b7b0f2a7df4c1dc1123ec19259cfff9e66
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-11-17 18:04:10 +0100

    fix_ex3

--- first_seminar.py
+++ first_seminar.py
@@ -20,9 +20,14 @@ print("rgb to YUV:", converter.from_rgb_to_yuv(255, 0, 0))  # Example
 print("YUV to rgb:", converter.from_yuv_to_rgb(162, 94, 224))  # Example 
 
 # Method defined following: https://smarttech101.com/ffmpeg-compress-rescale-video-and-image
-def resize_imge(input_iamge, output_image, width, height):  
-
-    command = ['ffmpeg' '-i', '{input_iamge}', '-filter:v', f'scale={width}:{height}' '{output_image}']
+def resize_image(input_image, output_image, width, height):
+    command = [
+        'ffmpeg',
+        '-y',
+        '-i', input_image,
+        '-vf', f'scale={width}:{height}',
+        output_image,
+    ]
     subprocess.run(command)
 
-resize_imge('input.jpg', 'output.jpg', 800, 600) # Example usage
+resize_image('input.jpg', 'output.jpg', 400, 300)  # Example usage
