1d931f22882a236ab8ae23445a580ef3d41df79a
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-11-17 18:28:09 +0100

    exercise 4 start

--- first_seminar.py
+++ first_seminar.py
@@ -31,3 +31,17 @@ def resize_image(input_image, output_image, width, height):
     subprocess.run(command)
 
 resize_image('input.jpg', 'output.jpg', 400, 300)  # Example usage
+#method defined following: https://yasoob.me/posts/understanding-and-writing-jpeg-decoder-in-python/
+
+def serpentine(file_path):
+    # Standard JPEG zigzag order
+    zigzag = [0,1,5,6,14,15,27,28,2,4,7,13,16,26,29,42,3,8,12,17,25,30,41,43,9,11,18,24,31,40,44,53,10,19,23,32,39,45,52,54,20,22,33,38,46,51,55,60,21,34,37,47,50,56,59,61,35,36,48,49,57,58,62,63]
+    with open(file_path, 'rb') as f:
+        data = f.read()
+        # Take first 64 bytes and read in zigzag order
+        result = [data[zigzag[i]] for i in range(64)]
+        return result
+
+serpentine("input.jpg")
+    
+    
